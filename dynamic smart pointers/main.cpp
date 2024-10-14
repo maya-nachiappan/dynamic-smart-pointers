3a605f3eb789fd83cd41c108ae4c6f3289caada8
maya-nachiappan <maya.n102304@gmail.com>
2024-10-14 11:55:58 -0700

    Initial Commit

---
+++ dynamic smart pointers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dynamic smart pointers
+//
+//  Created by maya nachiappan on 10/14/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
